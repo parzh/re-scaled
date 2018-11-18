f3cb667d66643319441b84aa7c2c4db393efddd2
Dmitry Parzhitsky <parzhitsky@gmail.com>
2018-11-18 19:10:50 +0200

    create unit tests for repeated.between()

---
+++ test/repeated/between.spec.ts
@@ -0,0 +1,15 @@
+import "mocha";
+import { expect } from "chai";
+import { combined, repeated } from "../../src";
+
+describe("repeated.atLeast()", () => {
+	const { source, flags } = combined(/a/g, repeated.between(3, 5)(/b/i));
+
+	it("Repeats pattern between `min` and `max` amount of times inclusively", () => {
+		expect(source).to.equal("a(?:b){3,5}");
+	});
+
+	it("Merges all flags from all RegExp inputs together", () => {
+		expect(flags).to.equal("gi");
+	});
+});
