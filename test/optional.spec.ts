97eda314b438cbb2f2475520355974f42633c732
Dmitry Parzhitsky <parzhitsky@gmail.com>
2018-11-18 18:24:16 +0200

    create unit tests for optional()

---
+++ test/optional.spec.ts
@@ -0,0 +1,15 @@
+import "mocha";
+import { expect } from "chai";
+import { optional } from "../src";
+
+describe("optional()", () => {
+	const { source, flags } = optional(/a/g, /b/i);
+
+	it("Makes pattern optional", () => {
+		expect(source).to.equal("(?:ab)?");
+	});
+
+	it("Merges all flags from all RegExp inputs together", () => {
+		expect(flags).to.equal("gi");
+	});
+});
