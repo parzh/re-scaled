5e217bd5d0a8507390fc8322f285d1b0271dc5d5
Dmitry Parzhitsky <parzhitsky@gmail.com>
2018-11-18 19:07:55 +0200

    create unit tests for repeated.times()

---
+++ test/repeated/times.spec.ts
@@ -0,0 +1,15 @@
+import "mocha";
+import { expect } from "chai";
+import { combined, repeated } from "../../src";
+
+describe("repeated.twice()", () => {
+	const { source, flags } = combined(/a/g, repeated.times(8)(/b/i));
+
+	it("Repeats pattern exactly `count` amount of times", () => {
+		expect(source).to.equal("a(?:b){8}");
+	});
+
+	it("Merges all flags from all RegExp inputs together", () => {
+		expect(flags).to.equal("gi");
+	});
+});
