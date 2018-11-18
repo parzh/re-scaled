4d38afb83c01e3f7387405f2988225bb1d2d9462
Dmitry Parzhitsky <parzhitsky@gmail.com>
2018-11-18 18:53:31 +0200

    create unit tests for eitherOf()

---
+++ test/either-of.spec.ts
@@ -0,0 +1,15 @@
+import "mocha";
+import { expect } from "chai";
+import { combined, eitherOf } from "../src";
+
+describe("eitherOf()", () => {
+	const { source, flags } = combined(/a/g, eitherOf(/b/i, /c/y));
+
+	it("Expects appearance of one of the given patterns", () => {
+		expect(source).to.equal("a(?:b|c)");
+	});
+
+	it("Merges all flags from all RegExp inputs together", () => {
+		expect(flags).to.equal("giy");
+	});
+});
