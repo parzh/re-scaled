3617563a20052b9d6fe537dab02d4fdc46c9967c
Dmitry Parzhitsky <parzhitsky@gmail.com>
2018-11-18 19:38:37 +0200

    create integration case "number"

---
+++ test/integration/number.spec.ts
@@ -0,0 +1,16 @@
+import "mocha";
+import { expect } from "chai";
+import { oneOrMore, storedAs, combined, detached, optional } from "../../src";
+
+describe("Integration case: number", () => {
+	it("Creates regexp for numbers with optional decimal part", () => {
+		const digits = oneOrMore(/\d/);
+		const decimalSeparator = /[.,]/;
+		const integerPart = storedAs("integer")(digits);
+		const decimalPart = combined(decimalSeparator, storedAs("decimal")(digits));
+	
+		const { source } = detached(integerPart, optional(decimalPart));
+	
+		expect(source).to.equal("^(?<integer>(?:\\d)+)(?:[.,](?<decimal>(?:\\d)+))?$");
+	});
+});
