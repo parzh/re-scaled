db7cac8222c90c572ca10c4beeb3873f53c9485e
Dmitry Parzhitsky <parzhitsky@gmail.com>
2019-01-27 13:28:37 +0200

    Identify bounds' values by order, instead of by position

--- src/modules/repeated/between.ts
+++ src/modules/repeated/between.ts
@@ -4,11 +4,11 @@ import { concat } from "../../helpers/concat";
 
 /** Repeat pattern between `min` and `max` amount of times inclusively */
 export function repeatedBetween(min: number, max: number) {
-	asNatural(min, "min repeat count");
-	asNatural(max, "max repeat count");
-
 	const _min = Math.min(min, max);
 	const _max = Math.max(min, max);
+
+	asNatural(_min, "min repeat count");
+	asNatural(_max, "max repeat count");
 
 	return (...patterns: Pattern[]): RegExp => concat(patterns, (descr) => ({
 		...descr,
