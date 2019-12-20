22df0501aca8d133b9c01d01d2071d0195075b86
Dmitry Parzhitsky <parzhitsky@gmail.com>
2019-12-20 22:58:53 +0200

    Improve the code of "localhost" integration test

--- test/integration/localhost.spec.ts
+++ test/integration/localhost.spec.ts
@@ -1,23 +1,22 @@
 import "mocha";
 import { expect } from "chai";
-import { separatedBy, detached, storedAs, eitherOf, combined, optional, repeated } from "../../src";
+import { combined, detached, storedAs, eitherOf, repeatedBetween, separatedBy } from "../../src";
 
 describe("Integration case: Localhost", () => {
 	it("Creates regexp for localhost hostname", () => {
-		const $OctetHigh = /25[0-5]/;
-		const $OctetMid = /2[0-4]\d/;
-		const $OctetLow = combined(optional(eitherOf(0, 1)), repeated.between(1, 2)(/\d/));
-		const $Octet = eitherOf($OctetHigh, $OctetMid, $OctetLow);
+		const $Dot = ".";
+		const $Zero = repeatedBetween(1, 3)("0");
+		const $One = combined(repeatedBetween(0, 2)("0"), "1");
 
-		const $Dot = /\./;
 		const $LocalhostIpV6 = /\[::1\]/;
-		const $LocalhostIpV4 = separatedBy($Dot)(/127/, $Octet, $Octet, $Octet);
+		const $LocalhostIpV4 = separatedBy($Dot)("127", $Zero, $Zero, $One);
 
-		const { source } = detached(storedAs("hostname")(eitherOf("localhost", $LocalhostIpV6, $LocalhostIpV4)));
-		const { source: octetSource } = $Octet;
-		const dottedOctetSource = `\\.${ octetSource }`;
-		const expected = `^(?<hostname>(?:localhost|\\[::1\\]|(?:127${ dottedOctetSource.repeat(3) })))$`;
+		const { source: actual } =
+			detached(storedAs("hostname")(eitherOf("localhost", $LocalhostIpV6, $LocalhostIpV4)));
 
-		expect(source).to.equal(expected);
+		const expected =
+			`^(?<hostname>(?:localhost|\\[::1\\]|(?:127${ ($Dot + $Zero.source).repeat(2) }.${ $One.source })))$`;
+
+		expect(actual).to.equal(expected);
 	});
 });
